20cc66d78a222ed83e91d42f09efbc041fd4fa1a
Dean Hicks <hdean@google.com>
2023-01-31 23:49:36 +0000

    init commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider, GoogleLogin, googleLogout } from '@react-oauth/google';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const GoogleUserContext = React.createContext();
+
+function Welcome(props) {
+  return (
+    <GoogleLogin
+      onSuccess={credentialResponse => {
+        console.log(credentialResponse);
+      }}
+      onError={() => {
+        console.log('Login Failed');
+      }}
+    />
+  );
+}
+
+function Home(props) {
+  return (
+    <div>
+    home {React.useContext(GoogleUserContext)}
+    </div>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Welcome/>,
+  },
+  {
+    path: "/home",
+    element: <Home/>,
+  },
+]);
+
+const googleClientId = `${process.env.REACT_APP_GOOGLE_CLIENT_ID}.apps.googleusercontent.com`;
+
+const root = ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <GoogleUserContext.Provider value="Reed">
+      <GoogleOAuthProvider clientId={googleClientId}>
+        <RouterProvider router={router} />
+      </GoogleOAuthProvider>
+    </GoogleUserContext.Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+fetch('/blah')
+.then((response) => response.json())
+.then((data) => alert(data));
