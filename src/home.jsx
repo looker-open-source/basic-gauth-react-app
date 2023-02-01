db35687685208a49eb2cb4b23e7511086d6b153c
Jeremy Chang <jeremytchang@google.com>
2023-02-01 18:40:13 +0000

    Added comments

--- src/home.jsx
+++ src/home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { GoogleUserContext } from '.';
 
+// This page is on https://embed.demo.com:3000
 export const Home = (props) => {
     // This is your user's ID in the app.
     const [googleUserId] = React.useContext(GoogleUserContext)
